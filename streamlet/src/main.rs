b81ea40ebac16a4080086988bba51cd75666a919
Arden Ma <ardenma@stanford.edu>
2022-05-06 20:17:55 -0700

    added basic digital signature functionality for streamlet

--- streamlet/src/main.rs
+++ streamlet/src/main.rs
@@ -2,7 +2,29 @@ mod utils;
 
 use utils::{Block, Vote, Sha256Hash};
 use sha2::{Sha256, Digest};
+use rand::rngs::OsRng;
+use ed25519_dalek::{Keypair, PublicKey, Signature, Signer, Verifier};
 
+struct StreamletInstance {
+    keypair: Keypair,
+}
+impl StreamletInstance {
+    pub fn get_public_key(&self) -> PublicKey {
+        return self.keypair.public;
+    }
+    pub fn sign(&self, message: &[u8]) -> Signature {
+        return self.keypair.sign(message);
+    }
+}
+
+
+fn create_streamlet_instance() -> StreamletInstance {
+    // Setup public/private key pair
+    let mut csprng = OsRng{};
+    let keypair: Keypair = Keypair::generate(&mut csprng);
+
+    return StreamletInstance {keypair: keypair};
+}
 
 fn main() {
     // create a Sha256 object
@@ -33,4 +55,11 @@ fn main() {
     println!("{:?}", blk2.hash());
     println!("{:?}", blk1.hash() == blk2.hash());
     println!("{:?}", blk2.hash() == blk3.hash());
+
+    let streamlet: StreamletInstance = create_streamlet_instance();
+
+    let message: &[u8] = b"This is a test of the tsunami alert system.";
+    let signature: Signature = streamlet.sign(message);
+    let public_key: PublicKey = streamlet.get_public_key();
+    assert!(public_key.verify(message, &signature).is_ok());
 }
