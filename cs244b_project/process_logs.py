c27e90c33a8f5086b2de4ad5e09107c590e17c39
Arden Ma <ardenma@stanford.edu>
2022-06-03 12:21:39 -0700

    update benchmark code

--- cs244b_project/process_logs.py
+++ cs244b_project/process_logs.py
@@ -1,18 +1,27 @@
 import os
 
+import matplotlib
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-LOGDIR = "./normal_logs"
+NORMAL_LOGDIR = "./normal_logs"
+COMPROMISED_LOGDIR = "./compromised_logs"
 PLOTDIR = "./plots"
+
+
+matplotlib.rcParams.update({'font.size': 20, 'axes.titlepad': 20})
+matplotlib.rc('xtick', labelsize=16) 
+matplotlib.rc('ytick', labelsize=16) 
 
 
 os.makedirs(PLOTDIR, exist_ok=True)
 
-all_df_list = []
+############################################### Normal Behavior #######################################################
 
-for dirname in os.scandir(LOGDIR):
+all_df_list = []
+for dirname in os.scandir(NORMAL_LOGDIR):
     if dirname.is_dir():
         num_nodes = os.path.basename(dirname)[0]
         df_list = []
@@ -32,48 +41,90 @@ df["epoch_finalization_percentage"] = df["num_finalized"] / 19 * 100
 print(df.to_string())
 
 ############################################### Percentage of Epoch Finalizations #######################################################
-fig, axes = plt.subplots(1, 3, figsize=(16, 4))
-fig.tight_layout(pad=4)
-plt.suptitle("% of Epochs with Finalized Block")
+# fig, axes = plt.subplots(1, 3, figsize=(16, 4))
+# fig.tight_layout(pad=4)
+# plt.suptitle("% of Epochs with Finalized Block")
+
+figsize = (10,8)
+
+plt.figure(figsize=figsize)
 
 df_epoch_1_data_small = df[(df["epoch_length"] == 1) & (df["data_type"] == "small")]
 df_epoch_5_data_small = df[(df["epoch_length"] == 5) & (df["data_type"] == "small")]
 df_epoch_10_data_small = df[(df["epoch_length"] == 10) & (df["data_type"] == "small")]
 
-sns.lineplot(ax=axes[0], x=df_epoch_1_data_small["num_nodes"], y=df_epoch_1_data_small["epoch_finalization_percentage"]);
-sns.lineplot(ax=axes[0], x=df_epoch_5_data_small["num_nodes"], y=df_epoch_5_data_small["epoch_finalization_percentage"]);
-sns.lineplot(ax=axes[0], x=df_epoch_10_data_small["num_nodes"], y=df_epoch_10_data_small["epoch_finalization_percentage"]);
+sns.lineplot(x=df_epoch_1_data_small["num_nodes"], y=df_epoch_1_data_small["epoch_finalization_percentage"]);
+sns.lineplot(x=df_epoch_5_data_small["num_nodes"], y=df_epoch_5_data_small["epoch_finalization_percentage"]);
+sns.lineplot(x=df_epoch_10_data_small["num_nodes"], y=df_epoch_10_data_small["epoch_finalization_percentage"]);
 
-axes[0].set_title("1B Block")
-axes[0].set_xlabel("Number of Streamlet Instances")
-axes[0].set_ylabel("% of Epochs with Finalized Block")
-axes[0].legend(labels=["1s Epoch", "5s Epoch", "10s Epoch"])
+plt.title("% of Epochs with Finalize Block (1B Block)")
+plt.xlabel("Number of Streamlet Instances")
+plt.ylabel("% of Epochs with Finalized Block")
+plt.legend(labels=["1s Epoch", "5s Epoch", "10s Epoch"])
+plt.savefig(os.path.join(PLOTDIR, "1b.png"))
+
+plt.figure(figsize=figsize)
 
 df_epoch_1_data_medium = df[(df["epoch_length"] == 1) & (df["data_type"] == "medium")]
 df_epoch_5_data_medium = df[(df["epoch_length"] == 5) & (df["data_type"] == "medium")]
 df_epoch_10_data_medium = df[(df["epoch_length"] == 10) & (df["data_type"] == "medium")]
 
-sns.lineplot(ax=axes[1], x=df_epoch_1_data_medium["num_nodes"], y=df_epoch_1_data_medium["epoch_finalization_percentage"]);
-sns.lineplot(ax=axes[1], x=df_epoch_5_data_medium["num_nodes"], y=df_epoch_5_data_medium["epoch_finalization_percentage"]);
-sns.lineplot(ax=axes[1], x=df_epoch_10_data_medium["num_nodes"], y=df_epoch_10_data_medium["epoch_finalization_percentage"]);
+sns.lineplot(x=df_epoch_1_data_medium["num_nodes"], y=df_epoch_1_data_medium["epoch_finalization_percentage"]);
+sns.lineplot(x=df_epoch_5_data_medium["num_nodes"], y=df_epoch_5_data_medium["epoch_finalization_percentage"]);
+sns.lineplot(x=df_epoch_10_data_medium["num_nodes"], y=df_epoch_10_data_medium["epoch_finalization_percentage"]);
 
-axes[1].set_title("1KB Block")
-axes[1].set_xlabel("Number of Streamlet Instances")
-axes[1].set_ylabel("% of Epochs with Finalized Block")
-axes[1].legend(labels=["1s Epoch", "5s Epoch", "10s Epoch"])
+plt.title("% of Epochs with Finalize Block (1KB Block)")
+plt.xlabel("Number of Streamlet Instances")
+plt.ylabel("% of Epochs with Finalized Block")
+plt.legend(labels=["1s Epoch", "5s Epoch", "10s Epoch"])
+plt.savefig(os.path.join(PLOTDIR, "1kb.png"))
+
+plt.figure(figsize=figsize)
 
 df_epoch_1_data_large = df[(df["epoch_length"] == 1) & (df["data_type"] == "large")]
 df_epoch_5_data_large = df[(df["epoch_length"] == 5) & (df["data_type"] == "large")]
 df_epoch_10_data_large = df[(df["epoch_length"] == 10) & (df["data_type"] == "large")]
 
-sns.lineplot(ax=axes[2], x=df_epoch_1_data_large["num_nodes"], y=df_epoch_1_data_large["epoch_finalization_percentage"]);
-sns.lineplot(ax=axes[2], x=df_epoch_5_data_large["num_nodes"], y=df_epoch_5_data_large["epoch_finalization_percentage"]);
-sns.lineplot(ax=axes[2], x=df_epoch_10_data_large["num_nodes"], y=df_epoch_10_data_large["epoch_finalization_percentage"]);
+sns.lineplot(x=df_epoch_1_data_large["num_nodes"], y=df_epoch_1_data_large["epoch_finalization_percentage"]);
+sns.lineplot(x=df_epoch_5_data_large["num_nodes"], y=df_epoch_5_data_large["epoch_finalization_percentage"]);
+sns.lineplot(x=df_epoch_10_data_large["num_nodes"], y=df_epoch_10_data_large["epoch_finalization_percentage"]);
 
-axes[2].set_title("10KB Block")
-axes[2].set_xlabel("Number of Streamlet Instances")
-axes[2].set_ylabel("% of Epochs with Finalized Block")
-axes[2].legend(labels=["1s Epoch", "5s Epoch", "10s Epoch"])
+plt.title("% of Epochs with Finalize Block (10KB Block)")
+plt.xlabel("Number of Streamlet Instances")
+plt.ylabel("% of Epochs with Finalized Block")
+plt.legend(labels=["1s Epoch", "5s Epoch", "10s Epoch"])
+plt.savefig(os.path.join(PLOTDIR, "10kb.png"))
 
-plt.savefig(os.path.join(PLOTDIR, "percentage_of_epochs_with_finalizations.png"))
+############################################### Compromised Behavior #######################################################
+NUM_FLAGS = 6
+NUM_TRIALS = 1
+COMPROMISE_TYPES = ["early_epoch", "late_epoch", "no_propose", "wrong_parent_hash", "no_vote", "non_leader_propose"]
+
+all_df_list = []
+for dirname in os.scandir(COMPROMISED_LOGDIR):
+    if dirname.is_dir():
+        num_nodes = int(os.path.basename(dirname)[0])
+        results_list = []
+        
+        # Loop through all files in a directory
+        for filename in os.scandir(dirname):
+            if os.path.basename(filename) != f"Test{num_nodes -1}.log":  # compromised node
+                results = [0, 0, 0, 0, 0, 0]
+                df = pd.read_csv(filename, index_col=None, header=None)
+                df.columns = ["num_nodes", "epoch_length", "data_type", "num_finalized", "finalization_percentage", "compromised_flags"]
+                for i, n in enumerate(df["num_finalized"]):
+                    results[i % 6] += n
+                results = list(map(lambda x: x / NUM_TRIALS, results))
+                results_list.append(results)
+
+        results = np.mean(results_list, axis=0) / 19 * 100
+        print(results)
+
+    plt.figure(figsize=(12,6))
+    sns.barplot(x=COMPROMISE_TYPES, y=results)
+    plt.title(f"Streamlet Finalization Performance with {num_nodes-1} Honest Nodes and 1 Malicious Node")
+    plt.xlabel("Compromise Type")
+    plt.ylabel("% of Epochs with Finalized Block")
+    plt.savefig(os.path.join(PLOTDIR, f"{num_nodes}_nodes_compromise_types.png"))
+
 #######################################################################################################################################
--- cs244b_project/test_driver.py
+++ cs244b_project/test_driver.py
@@ -4,28 +4,28 @@ import itertools
 from subprocess import Popen, PIPE, STDOUT
 from tqdm import tqdm
 
-num_instances = [4, 10, 20]
+num_instances = [4, 5, 6, 7, 8, 9, 10]
 # epoch_lengths = [10, 20]
 epoch_lengths = [1, 5, 10]
 data_sizes = ['small', 'medium', 'large']
 
 # Test normal case
-# for i, (n, e, d) in tqdm(enumerate(itertools.product(num_instances, epoch_lengths, data_sizes))):
-#     os.makedirs(f"./logs/{n}_nodes", exist_ok=True)
-#     print(f"\nStarting iteration {i}. (num_instances={n}, epoch_length={e}, data_size={d})\n")
-#     p = Popen(['./target/debug/cs244b_project', f'{n}', f'bench', f'{e}', f'{d}', 'benchmark'], stdout=sys.stdout, stderr=sys.stderr)    
-#     status = p.wait()
-#     print(f"\nIteration {i} completed with status {status}. (num_instances={n}, epoch_length={e}, data_size={d})\n")
-
-
-compromise_flags = ['e', 'l', 'p', 'h', 'v', 'n']
-num_compromised = [1, 2]
-
-# Test compromised nodes
-for i, (nc, flag) in tqdm(enumerate(itertools.product(num_compromised, compromise_flags))):
-    os.makedirs(f"./logs/4_nodes", exist_ok=True)
-    print(f"\nStarting iteration {i}. (flag={flag}, num_compromised={nc})\n")
-    p = Popen(['./target/debug/cs244b_project', f'{4}', f'bench', f'{1}', 'small', f'{flag}', 'benchmark', f'{nc}'], stdout=sys.stdout, stderr=sys.stderr)    
+for i, (n, e, d) in tqdm(enumerate(itertools.product(num_instances, epoch_lengths, data_sizes))):
+    os.makedirs(f"./logs/{n}_nodes", exist_ok=True)
+    print(f"\nStarting iteration {i}. (num_instances={n}, epoch_length={e}, data_size={d})\n")
+    p = Popen(['./target/debug/cs244b_project', f'{n}', f'bench', f'{e}', f'{d}', 'benchmark'], stdout=sys.stdout, stderr=sys.stderr)    
     status = p.wait()
-    print(f"\nIteration {i} completed with status {status}. (flag={flag}, num_compromised={nc})\n")
+    print(f"\nIteration {i} completed with status {status}. (num_instances={n}, epoch_length={e}, data_size={d})\n")
+
+
+# compromise_flags = ['e', 'l', 'p', 'h', 'v', 'n']
+# num_compromised = [1]
+
+# # Test compromised nodes
+# for i, (n, nc, flag) in tqdm(enumerate(itertools.product(num_instances, num_compromised, compromise_flags))):
+#     os.makedirs(f"./logs/{n}_nodes", exist_ok=True)
+#     print(f"\nStarting iteration {i}. (flag={flag}, num_compromised={nc})\n")
+#     p = Popen(['./target/debug/cs244b_project', f'{n}', f'bench', f'{1}', 'small', f'{flag}', 'benchmark', f'{nc}'], stdout=sys.stdout, stderr=sys.stderr)    
+#     status = p.wait()
+#     print(f"\nIteration {i} completed with status {status}. (flag={flag}, num_compromised={nc})\n")
 
